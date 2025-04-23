e1285c6933d8f2191d3c340ad12f9e0b3aab2f09
Semmel978 <samuel.kaspar2007@gmx.at>
2025-04-23 11:32:44 +0200

    frwbvfr

--- Walch_Projekt.py
+++ Walch_Projekt.py
@@ -1,2 +1,43 @@
-#hi, geht es jetzt? (enis)
-print("hello world")
+class row():
+    def __init__(self):
+        self.points = ["empty","empty","empty"]
+    
+    def change_point(self, point, new_value):
+        self.points[point] = new_value
+
+class board():
+    def __init__(self):
+        self.points = []
+        for i in range(24):
+            self.points.append("empty")
+        self.rows = {"top_top":row(),
+                     "top_mid":row(),
+                     "top_bottom":row(),
+                     "top_out":row(),
+                     "left_left":row(),
+                     "left_mid":row(),
+                     "left_right":row(),
+                     "left_out":row(),
+                     "bottom_top":row(),
+                     "bottom_mid":row(),
+                     "bottom_bottom":row(),
+                     "bottom_out":row(),
+                     "right_left":row(),
+                     "right_mid":row(),
+                     "right_right":row(),
+                     "right_out":row()}
+        self.points_dict = {
+            0: [["top_top", 0], "left_left"],
+            1: ["top_top", "top_out"],
+            2: ["top_top", "right_right"],
+            3: ["left_mid", "top_mid"],
+            4: ["top_mid", "top_out"],
+            5: ["right_mid", "top_mid"],
+            6: ["left_right", "top_bottom"],
+            7: ["top_bottom", "top_out"],
+            8: ["top_bottom", "right_left"],
+            9: ["left_left", "left_out"],
+            10: ["left_mid", "left_out"],
+            11: ["left_right", "left_out"],
+            12: ["right_"]
+        }                 
